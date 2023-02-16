41f0c21c197f7bee63d4e453b44ac24d00b4d966
renoker <rodolfoulises.ramirez@gmail.com>
2023-02-15 20:01:22 -0600

    Primera carga estilos

---
+++ resources/js/global.js
@@ -0,0 +1,3 @@
+import '../scss/global.scss';
+import '../scss/commons/header.scss';
+import '../scss/commons/footer.scss';
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [                
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                // JS                 
+                'resources/js/global.js',
+                'resources/js/home.js',
+                // SCSS
+                'resources/scss/home.scss',
+                'resources/scss/commons/header.scss',
+                'resources/scss/commons/footer.scss',                
+            ],
             refresh: true,
         }),
     ],
